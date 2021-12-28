b6f03ee8db0386532b0b53e20da7f16a5a2446a0
Brit Wallace <britwallace888@gmail.com>
2021-12-27 18:02:25 -0800

    Red phase in testing

---
+++ _tests_/mercury.test.js
@@ -0,0 +1,7 @@
+import mercury from '../src/mercury.js'
+
+describe('Mercury'), () => {
+  test('should divide user input by 365'), () => {
+  let inputAge = ("");
+  const mercuryDays = new Age(inputAge / 365);
+}};
