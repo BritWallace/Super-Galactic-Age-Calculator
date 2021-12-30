07659fce6d981a62e42a51df0b9319ea726f39b6
Brit Wallace <britwallace888@gmail.com>
2021-12-29 23:03:56 -0800

    test fails to show  Mercury age

--- _tests_/planet.test.js
+++ _tests_/planet.test.js
@@ -3,22 +3,34 @@ describe('Human', () => {
   let reusableHuman;
 
   beforeEach(() => {
-    reusableHuman = new Human(20,"female");
+    reusableHuman = new Human(20,72);
+  
   });
 
   test('should show how beforeEach() works', () => {
     console.log(reusableHuman);
   });
 
-  test('should see a users age and gender', () => {
+  test('should see a users age and expected life expectancy', () => {
     expect(reusableHuman.age).toEqual(20);
-    expect(reusableHuman.gender).toEqual('female');
-
-  test('should determine if the user inputs a positive number', () => {
-    expect(reusableHuman.age).filter(num => num > 1 && num % parseInt(num) === 1);
-  });
+    expect(reusableHuman.maxAge).toEqual(72);
 
   }); 
+
+  test('should show earth years in solar Mercury years', () => {
+    const age = new age(20);
+    const mercuryAge = age.toMercuryAge();
+    expect(reusableHuman.age).toEqual(20);
+    expect(mercuryAge).toEqual(20 / .24);
+  })
+
+  
+ 
+  // test('should determine if the user inputs a positive number', () => {
+  //   expect(reusableHuman.age).filter(num => num > 1 && num % parseInt(num) === 1);
+  // });
+
+  
 });
 
 // describe('Human', () => {
@@ -59,7 +71,7 @@ describe('Human', () => {
 
 //  1 get a users age 
 //  2 get a positive number 
-//  3 male 73 / female 80 / father
+//  3 male 73 / female 80 / 
 //  4 if else male and =>74 return years lived past life male life expectancy
 //  5 if female and => 80 ect...
 //  6 method to the class for the above 
